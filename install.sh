5b360da23276745f1667b58c0667e49288750e92
Ashley <ashley.damiens@epitech.eu>
2022-02-26 18:24:23 +0100

    made install.sh prettier, thanks Ange, I was lazy :)

--- install.sh
+++ install.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
+set -e
 
-mkdir -p /home/$(whoami)/.config/systemd/user/
+user="$(whoami)"
+
+mkdir -p "$HOME"/.config/systemd/user/
 git clone https://github.com/AshleyTheNeko/FumoScreen.git /tmp/fumo
 cd /tmp/fumo
-sed -e "s/ashley/$(whoami)/g" default.cpp > fumo.cpp
-mv assets/cirno.png /home/$(whoami)/.config/systemd/user/
+sed -e "s/ashley/$user/g" default.cpp > fumo.cpp
+mv assets/cirno.png "$HOME"/.config/systemd/user/
 make
-mv fumo /home/$(whoami)/.config/systemd/user/
-sed -e "s/ashley/$(whoami)/g" default_service > fumo.service
-mv fumo.service /home/$(whoami)/.config/systemd/user/
+mv fumo "$HOME"/.config/systemd/user/
+sed -e "s/ashley/$user/g" default_service > fumo.service
+mv fumo.service "$HOME"/.config/systemd/user/
 systemctl --user enable fumo.service
