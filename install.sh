04f75b4ebb236fb1916976474a4c74798a60e1a9
Ashley <ashley.damiens@epitech.eu>
2022-02-28 16:59:33 +0100

    -f

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ mkdir -p "$HOME"/.config/systemd/user/
 git clone https://github.com/AshleyTheNeko/FumoScreen.git /tmp/fumo
 cd /tmp/fumo
 sed -e "s/ashley/$user/g" default.cpp > fumo.cpp
-mv assets "$HOME"/.config/systemd/user/
+mv -f assets "$HOME"/.config/systemd/user/
 make
-mv fumo "$HOME"/.config/systemd/user/
+mv -f fumo "$HOME"/.config/systemd/user/
 sed -e "s/ashley/$user/g" default_service > fumo.service
-mv fumo.service "$HOME"/.config/systemd/user/
+mv -f fumo.service "$HOME"/.config/systemd/user/
 systemctl --user enable fumo.service
