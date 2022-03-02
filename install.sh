0a538794a6892c93ed74d9058a6c44e8b815f72f
Ashley <ashley.damiens@epitech.eu>
2022-03-02 14:16:05 +0100

    Added sfml autoinstall

--- install.sh
+++ install.sh
@@ -9,13 +9,14 @@ mkdir -p "$HOME"/.config/systemd/user/
 rm -rf /tmp/fumo
 rm -rf "$HOME"/.config/systemd/user/assets "$HOME"/.config/systemd/user/fumo "$HOME"/.config/systemd/user/fumo.service
 
+
+git clone https://github.com/AshleyTheNeko/FumoScreen.git /tmp/fumo
+cd /tmp/fumo
+
 wget https://www.sfml-dev.org/files/SFML-2.5.1-linux-gcc-64-bit.tar.gz
 tar -xf *tar.gz
 mv -f SFML*/include/* includes
 mv -f SFML*/lib .
-
-git clone https://github.com/AshleyTheNeko/FumoScreen.git /tmp/fumo
-cd /tmp/fumo
 
 sed -e "s/ashley/$user/g" default.cpp > fumo.cpp
 make
